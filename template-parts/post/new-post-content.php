931c0ed81f6c19edc54471e1b83c852de8d00c39
TungDS <tungdsse04019@fpt.edu.vn>
2018-02-07 22:45:40 +0700

    Fix post link

--- template-parts/post/new-post-content.php
+++ template-parts/post/new-post-content.php
@@ -1,11 +1,11 @@
 <div class="product column column-block">
     <div class="image">
-        <a href="<?php esc_url(get_permalink()) ?>">
+        <a href="<?php echo esc_url(get_permalink()) ?>">
             <?php the_post_thumbnail('shop_catalog') ?>
         </a>
     </div>
     <div class="product-name">
-        <a href="<?php esc_url(get_permalink()) ?>">
+        <a href="<?php echo esc_url(get_permalink()) ?>">
             <?php the_title() ?> </a>
     </div>
 </div>
